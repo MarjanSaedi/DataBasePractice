425993bdb045e622db42277fe9b745ea80fd0f6b
MarjanSaedi <115734478+MarjanSaedi@users.noreply.github.com>
2022-10-17 12:36:01 -0600

    Update Shopping.sql

--- Shopping.sql
+++ Shopping.sql
@@ -117,3 +117,61 @@ VALUES ('2016/02/10', 99.99, 1),
        
 	delete from customers where fname = 'Boy';
     select * from orders;
+    
+## School Database
+
+create database School;
+use School;
+select database();
+create table students (
+id int auto_increment primary key,
+first_name varchar(100));
+
+CREATE TABLE papers (
+    title VARCHAR(100),
+    grade INT,
+    student_id INT,
+    FOREIGN KEY (student_id) 
+        REFERENCES students(id)
+        ON DELETE CASCADE
+);
+
+INSERT INTO students (first_name) VALUES 
+('Caleb'), 
+('Samantha'), 
+('Raj'), 
+('Carlos'), 
+('Lisa');
+ 
+INSERT INTO papers (student_id, title, grade ) VALUES
+(1, 'My First Book Report', 60),
+(1, 'My Second Book Report', 75),
+(2, 'Russian Lit Through The Ages', 94),
+(2, 'De Montaigne and The Art of The Essay', 98),
+(4, 'Borges and Magical Realism', 89);
+
+select first_name, title, grade from students left join papers on
+students.id = papers.student_id;
+
+select first_name,
+ifnull(title, 'missing'),
+ifnull(grade, 0)
+from students left join papers on
+students.id = papers.student_id;
+
+select first_name,
+ifnull(avg(grade),0) as 'average'
+from students left join papers on
+students.id = papers.student_id
+group by students.id
+order by average desc;
+
+select first_name,
+ifnull(avg(grade),0) as 'average',
+case when avg(grade) > 80 then 'passed'
+else 'failed'
+end as 'pass_status' 
+from students left join papers on
+students.id = papers.student_id
+group by students.id
+order by average desc;
