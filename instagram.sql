678be1eda067e813ecdb3e0068d801db1bc1c8bb
MarjanSaedi <115734478+MarjanSaedi@users.noreply.github.com>
2022-10-26 13:29:36 -0600

    Update instagram.sql

--- instagram.sql
+++ instagram.sql
@@ -76,9 +76,38 @@ ORDER BY total DESC
 LIMIT 2;
 
 # Identify Inactive Users (users with no photos)
-
 SELECT username
 FROM users
 LEFT JOIN photos
     ON users.id = photos.user_id
 WHERE photos.id IS NULL;
+
+#Identify most popular photo (and user who created it)
+SELECT 
+    username,
+    photos.id,
+    photos.image_url, 
+    COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
+
+#Calculate average number of photos per user
+SELECT (SELECT Count(*) 
+        FROM   photos) / (SELECT Count(*) 
+                          FROM   users) AS avg; 
+                          
+#Find the five most popular hashtags
+SELECT tags.tag_name, 
+       Count(*) AS total 
+FROM   photo_tags 
+       JOIN tags 
+         ON photo_tags.tag_id = tags.id 
+GROUP  BY tags.id 
+ORDER  BY total DESC 
+LIMIT  5; 
